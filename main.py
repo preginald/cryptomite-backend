cd3ecc94eca4b9fe9220659a9ca18d0a52994c75
preginald <peter@reginald.com.au>
2021-11-01 19:27:41 +1100

    Add new routes for testing functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from test import testing, csvToJson
+from test import testing, csvToJson, getToken
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import calculators
@@ -10,32 +10,32 @@ app.config.from_object(__name__)
 CORS(app)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def greetings():
     return "Hello, world!"
 
 
-@app.route('/simple-calculator', methods=['POST'])
+@app.route("/simple-calculator", methods=["POST"])
 def simple_calc():
     return calculators.simple_calc()
 
 
-@app.route('/calculateDepositFee', methods=['POST'])
+@app.route("/calculateDepositFee", methods=["POST"])
 def calculate_deposit_fee():
     deposit_fee_value = 0
     deposit_fee_qty = 0
     principle_after_fee = 0
     token_a_qty_after_fee = 0
 
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.get_json()
 
         # Start pulling data from form input
-        deposit_fee = float(data['depositFee'])
-        principle = float(data['principle'])
-        token_a_price = float(data['tokenAPrice'])
-        token_a_qty = float(data['tokenAQty'])
-        status = 'success'
+        deposit_fee = float(data["depositFee"])
+        principle = float(data["principle"])
+        token_a_price = float(data["tokenAPrice"])
+        token_a_qty = float(data["tokenAQty"])
+        status = "success"
 
         if deposit_fee > 0 and principle > 0:
             deposit_fee_value = principle * (deposit_fee / 100)
@@ -45,29 +45,31 @@ def calculate_deposit_fee():
             deposit_fee_qty = deposit_fee_value / token_a_price
             token_a_qty_after_fee = token_a_qty - deposit_fee_qty
 
-        return jsonify({
-            'depositFee': deposit_fee,
-            'principle': principle,
-            'tokenAPrice': token_a_price,
-            'depositFeeValue': deposit_fee_value,
-            'depositFeeQty': deposit_fee_qty,
-            'principleAfterFee': principle_after_fee,
-            'tokenAQtyAfterFee': token_a_qty_after_fee,
-            'status': status
-        })
+        return jsonify(
+            {
+                "depositFee": deposit_fee,
+                "principle": principle,
+                "tokenAPrice": token_a_price,
+                "depositFeeValue": deposit_fee_value,
+                "depositFeeQty": deposit_fee_qty,
+                "principleAfterFee": principle_after_fee,
+                "tokenAQtyAfterFee": token_a_qty_after_fee,
+                "status": status,
+            }
+        )
 
 
-@app.route('/calculateLP', methods=['POST'])
+@app.route("/calculateLP", methods=["POST"])
 def calculate_lp():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.get_json()
 
         # Start pulling data from form input
-        token_a_price = float(data['tokenAPrice'])
-        token_b_price = float(data['tokenBPrice'])
-        principle = float(data['principle'])
-        yield_farm = data['yieldFarm']
-        status = 'success'
+        token_a_price = float(data["tokenAPrice"])
+        token_b_price = float(data["tokenBPrice"])
+        principle = float(data["principle"])
+        yield_farm = data["yieldFarm"]
+        status = "success"
         token_a_qty = 0
         token_b_qty = 0
 
@@ -79,10 +81,12 @@ def calculate_lp():
             if principle > 0 and token_a_price > 0:
                 token_a_qty = principle / token_a_price
 
-        return jsonify({'tokenAQty': token_a_qty, 'tokenBQty': token_b_qty, 'status': status})
+        return jsonify(
+            {"tokenAQty": token_a_qty, "tokenBQty": token_b_qty, "status": status}
+        )
 
 
-@app.route('/calculateYield', methods=['POST'])
+@app.route("/calculateYield", methods=["POST"])
 def calculate_yield():
     total_yield = 0
     daily_yield = 0
@@ -97,15 +101,15 @@ def calculate_yield():
     n = 365
     t = 1
 
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.get_json()
 
         # Start pulling data from form input
-        apr = float(data['apr'])
-        token_a_price = float(data['tokenAPrice'])
-        principle = float(data['principle'])
-        deposit_fee_value = float(data['depositFeeValue'])
-        status = 'success'
+        apr = float(data["apr"])
+        token_a_price = float(data["tokenAPrice"])
+        principle = float(data["principle"])
+        deposit_fee_value = float(data["depositFeeValue"])
+        status = "success"
 
         if apr > 0 and principle > 0:
             # total_yield_value = principle * (apr/100)
@@ -124,22 +128,24 @@ def calculate_yield():
             fee_breakeven_days = fee_breakeven_hours / 24
             print(fee_breakeven_hours)
 
-        return jsonify({
-            'totalYield': total_yield,
-            'dailyYield': daily_yield,
-            'hourlyYield': hourly_yield,
-            'totalYieldValue': total_yield_value,
-            'dailyYieldValue': daily_yield_value,
-            'hourlyYieldValue': hourly_yield_value,
-            'principleBreakevenHours': principle_breakeven_hours,
-            'principleBreakevenDays': principle_breakeven_days,
-            'feeBreakevenHours': fee_breakeven_hours,
-            'feeBreakevenDays': fee_breakeven_days,
-            'status': status
-        })
+        return jsonify(
+            {
+                "totalYield": total_yield,
+                "dailyYield": daily_yield,
+                "hourlyYield": hourly_yield,
+                "totalYieldValue": total_yield_value,
+                "dailyYieldValue": daily_yield_value,
+                "hourlyYieldValue": hourly_yield_value,
+                "principleBreakevenHours": principle_breakeven_hours,
+                "principleBreakevenDays": principle_breakeven_days,
+                "feeBreakevenHours": fee_breakeven_hours,
+                "feeBreakevenDays": fee_breakeven_days,
+                "status": status,
+            }
+        )
 
 
-@app.route('/calculatePrinciple', methods=['POST'])
+@app.route("/calculatePrinciple", methods=["POST"])
 def calculate_principle():
     total_yield = 0
     daily_yield = 0
@@ -148,14 +154,14 @@ def calculate_principle():
     principle = 0
     hourly_yield_value = 0
 
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.get_json()
 
         # Start pulling data from form input
-        apr = float(data['apr'])
-        daily_yield_value = float(data['dailyYieldValue'])
-        token_a_price = float(data['tokenAPrice'])
-        status = 'success'
+        apr = float(data["apr"])
+        daily_yield_value = float(data["dailyYieldValue"])
+        token_a_price = float(data["tokenAPrice"])
+        status = "success"
 
         if apr > 0 and daily_yield_value > 0:
             total_yield_value = daily_yield_value * 365
@@ -166,24 +172,31 @@ def calculate_principle():
             hourly_yield = hourly_yield_value / token_a_price
             principle = total_yield_value / (apr / 100)
 
-        return jsonify({
-            'totalYield': total_yield,
-            'dailyYield': daily_yield,
-            'hourlyYield': hourly_yield,
-            'totalYieldValue': total_yield_value,
-            'dailyYieldValue': daily_yield_value,
-            'hourlyYieldValue': hourly_yield_value,
-            'principle': principle,
-            'status': status
-        })
+        return jsonify(
+            {
+                "totalYield": total_yield,
+                "dailyYield": daily_yield,
+                "hourlyYield": hourly_yield,
+                "totalYieldValue": total_yield_value,
+                "dailyYieldValue": daily_yield_value,
+                "hourlyYieldValue": hourly_yield_value,
+                "principle": principle,
+                "status": status,
+            }
+        )
 
 
-@app.route('/test', methods=['POST'])
+@app.route("/test", methods=["POST"])
 def test():
     return testing()
 
 
-@app.route('/csvToJson', methods=['POST'])
+@app.route("/getToken", methods=["POST"])
+def get_token():
+    return getToken()
+
+
+@app.route("/csvToJson", methods=["POST"])
 def convertMe():
     json_string = csvToJson()
     # print(type(jsonify(json_string)))
